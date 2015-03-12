62db68a8d50a1d351fa3702d804b20bd2194a179
Anthony <aspears@gatech.edu>
2015-03-12 12:29:50 -0400

    Initial commit

---
+++ EssentialMatrix_example/CMakeFiles/GTSAMTest.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/temp/my-gtsam/EssentialMatrix_example/EssentialMatrixExample.cpp" "/home/temp/my-gtsam/EssentialMatrix_example/CMakeFiles/GTSAMTest.dir/EssentialMatrixExample.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/temp/gtsam-3.2.0"
+  "/home/temp/gtsam-3.2.0/build"
+  "/home/temp/gtsam-3.2.0/build/gtsam"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ EssentialMatrix_example/EssentialMatrixExample.cpp
@@ -0,0 +1,191 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation, 
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file OdometryExample.cpp
+ * @brief Simple robot motion example, with prior and two odometry measurements
+ * @author Frank Dellaert
+ */
+
+/**
+ * Example of a simple 2D localization example
+ *  - Robot poses are facing along the X axis (horizontal, to the right in 2D)
+ *  - The robot moves 2 meters each step
+ *  - We have full odometry between poses
+ */
+
+// We will use Pose2 variables (x, y, theta) to represent the robot positions
+#include <gtsam/geometry/Pose2.h>
+
+// In GTSAM, measurement functions are represented as 'factors'. Several common factors
+// have been provided with the library for solving robotics/SLAM/Bundle Adjustment problems.
+// Here we will use Between factors for the relative motion described by odometry measurements.
+// Also, we will initialize the robot at the origin using a Prior factor.
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+
+// When the factors are created, we will add them to a Factor Graph. As the factors we are using
+// are nonlinear factors, we will need a Nonlinear Factor Graph.
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+
+// Finally, once all of the factors have been added to our factor graph, we will want to
+// solve/optimize to graph to find the best (Maximum A Posteriori) set of variable values.
+// GTSAM includes several nonlinear optimizers to perform this step. Here we will use the
+// Levenberg-Marquardt solver
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+
+// Once the optimized values have been calculated, we can also calculate the marginal covariance
+// of desired variables
+#include <gtsam/nonlinear/Marginals.h>
+
+// The nonlinear solvers within GTSAM are iterative solvers, meaning they linearize the
+// nonlinear functions around an initial linearization point, then solve the linear system
+// to update the linearization point. This happens repeatedly until the solver converges
+// to a consistent set of variable values. This requires us to specify an initial guess
+// for each variable, held in a Values container.
+#include <gtsam/nonlinear/Values.h>
+
+
+//*************AMS ADDED**************//
+#define PI 3.14159265
+#include <gtsam/geometry/Rot3.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/EssentialMatrix.h>
+#include <gtsam/geometry/Unit3.h>
+#include <gtsam/slam/EssentialMatrixConstraint.h>
+
+using namespace std;
+using namespace gtsam;
+
+int main(int argc, char** argv) {
+
+  // Create an empty nonlinear factor graph
+  NonlinearFactorGraph graph;
+
+  double priorYaw= 0;
+  double priorPitch = 0;
+  double priorRoll = 0;
+  Rot3 priorMeanRot3 = Rot3::ypr(priorYaw, priorPitch, priorRoll);
+
+  cout << "Prior Rot Matrix:" << endl;
+  priorMeanRot3.print();
+  Vector3 current_ypr;
+  current_ypr = priorMeanRot3.ypr();
+  cout << "Prior Yaw = " << current_ypr[0] << endl;
+  cout << "Prior Pitch = " << current_ypr[1] << endl;
+  cout << "Prior Roll = " << current_ypr[2] << endl;
+  
+  Point3 priorMeanPoint3(0,0,0);
+  priorMeanPoint3.print("Prior 3D Point = ");
+
+  Pose3 priorMean(priorMeanRot3,priorMeanPoint3);
+
+  // Add a prior on the first pose, setting it to the origin
+  // A prior factor consists of a mean and a noise model (covariance matrix)
+  //  noiseModel::Diagonal::shared_ptr priorNoise = noiseModel::Diagonal::Sigmas((Vector(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+  noiseModel::Diagonal::shared_ptr priorNoise = noiseModel::Diagonal::Variances((Vector(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+
+  graph.add(PriorFactor<Pose3>(1, priorMean, priorNoise));
+
+  // Add odometry factors
+  Pose3 odometry_0(Rot3::ypr(0,0,0), Point3(0,0,0));
+  Pose3 odometry_x(Rot3::ypr(0,0,0), Point3(1,0,0));
+  Pose3 odometry_y(Rot3::ypr(0,0,0), Point3(0,1,0));
+  Pose3 odometry_z(Rot3::ypr(0,0,0), Point3(0,0,1));
+  Pose3 odometry_yaw(Rot3::ypr(PI/160,0,0), Point3(0,0,0));
+  //  odometry.print("Odometry=");
+
+  // For simplicity, we will use the same noise model for each odometry factor
+  //  noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Sigmas((Vector(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+  noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Variances((Vector(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+  // Create odometry (Between) factors between consecutive poses
+
+  Rot3 zeroRot3 = Rot3::ypr(0, 0, 0);
+  Unit3 xUnit3(1,0,0);
+  EssentialMatrix e_matrix(zeroRot3, xUnit3);
+
+  noiseModel::Diagonal::shared_ptr eMatNoise = noiseModel::Diagonal::Variances((Vector(5) << 0.1, 0.1, 0.1, 0.1, 0.1));
+
+  /*********************** Add Camera Nodes *************************/
+
+  int i;
+  for(i = 1; i<10; i++)
+    {
+      graph.add(EssentialMatrixConstraint(i, i+1, e_matrix, eMatNoise));
+    }
+  int n = i-1;
+
+  graph.print("\nFactor Graph:\n"); // print
+
+  // Create the data structure to hold the initialEstimate estimate to the solution
+  // For illustrative purposes, these have been deliberately set to incorrect values
+  Values initial;
+  double addedErr = 0;
+ 
+  initial.insert(1, Pose3(Rot3::ypr(addedErr,addedErr,addedErr), Point3(addedErr,addedErr,addedErr)));
+
+  for(i=2; i<11; i++)
+    {
+      initial.insert(i, Pose3(Rot3::ypr(0,0,0), Point3(i-1+addedErr,addedErr,addedErr)));
+    }
+  n=i-1;
+
+  cout << "Initial Estimate Nodes: " << n << endl;
+     
+  //  initial.print("\nInitial Estimate:\n"); // print
+
+  //AMS Custom Print:
+  for(i=1;i<n+1;i++)
+    {
+      double xprint = initial.at<Pose3>(i).x();
+      double yprint = initial.at<Pose3>(i).y();
+      double yawprint = initial.at<Pose3>(i).rotation().yaw();
+
+      if(abs(xprint) < 0.001)
+	xprint = 0;
+      if(abs(yprint) < 0.001)
+	yprint = 0;
+      if(abs(yawprint) < 0.001)
+	yawprint = 0;
+
+      cout << i << " Initial: x,y,yaw = " << xprint << "," << yprint << "," << yawprint << endl;
+    }
+
+  // optimize using Levenberg-Marquardt optimization
+  Values result = LevenbergMarquardtOptimizer(graph, initial).optimize();
+
+  // result.print("Final Result:\n");
+
+  for(i=1;i<n+1;i++)
+    {
+      double xprint = result.at<Pose3>(i).x();
+      double yprint = result.at<Pose3>(i).y();
+      double yawprint = result.at<Pose3>(i).rotation().yaw();
+
+      if(abs(xprint) < 0.001)
+	xprint = 0;
+      if(abs(yprint) < 0.001)
+	yprint = 0;
+      if(abs(yawprint) < 0.001)
+	yawprint = 0;
+
+      cout << i << " Result: x,y,yaw = " << xprint << "," << yprint << "," << yawprint << endl;
+    }
+
+  // Calculate and print marginal covariances for all variables
+  cout.precision(2);
+  Marginals marginals(graph, result);
+  cout << "x1 covariance:\n" << marginals.marginalCovariance(1) << endl;
+  cout << "x2 covariance:\n" << marginals.marginalCovariance(2) << endl;
+  cout << "x3 covariance:\n" << marginals.marginalCovariance(3) << endl;
+
+  return 0;
+}
