d654297614eeae0454165f562cb20b8e91a64087
Anthony <aspears@gatech.edu>
2015-04-13 12:33:16 -0400

    Added reading in files

--- cam_sonar_fusion/cam_sonar_fusion.cpp
+++ cam_sonar_fusion/cam_sonar_fusion.cpp
@@ -76,73 +76,185 @@ using namespace gtsam;
 
 int main(int argc, char** argv) 
 {
-  char inFileName[256];
+  char sonInputFileName[256];
+  char camInputFileName[256];
 
   if(argc < 2)
     {
-      cout << "Not Enough Args. Usage: ./gtsam <input CSV file with x,y,theta>" << endl;
+      cout << "Not Enough Args. Usage: ./gtsam <sonar input CSV file with x,y,theta> <camera input CSV file with x,y,z,roll,pitch,yaw>" << endl << "Example: ./gtsam soninput.csv caminput.csv" << endl;
+      return 0;
     }
   else
     {
-      strcpy(inFileName, argv[1]);  
+      strcpy(sonInputFileName, argv[1]);  
+      strcpy(camInputFileName, argv[2]);  
     }
 
-  ifstream inFile(inFileName);
+  ifstream inFileSon(sonInputFileName);
+  ifstream inFileCam(camInputFileName);
 
   string tmpstring;
-  //  getline(inFile,tmpstring,'\n');
-  getline(inFile,tmpstring,';');
-  cout << tmpstring << endl;
-  if(strcmp(tmpstring.c_str(),"x")==0) //FOUND x HEADING
-    cout << "FOUND X HEADING!" << endl;
+
+  getline(inFileSon,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"t1")==0) //FOUND t1 HEADING
+    cout << "FOUND t1 HEADING! - " << tmpstring << endl;
   else
-    cout << "DIDN'T FIND X HEADING!" << endl;
+    cout << "DIDN'T FIND t1 HEADING! - " << tmpstring << endl;
 
-  getline(inFile,tmpstring,';');
-  cout << tmpstring << endl;
-  if(strcmp(tmpstring.c_str(),"y")==0) //FOUND y HEADING
-    cout << "FOUND Y HEADING!" << endl;
+  getline(inFileSon,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"t2")==0) //FOUND t2 HEADING
+    cout << "FOUND t2 HEADING! - " << tmpstring << endl;
   else
-    cout << "DIDN'T FIND Y HEADING!" << endl;
+    cout << "DIDN'T FIND t2 HEADING! - " << tmpstring << endl;
 
-  getline(inFile,tmpstring,';');
-  cout << tmpstring << endl;
-  if(strcmp(tmpstring.c_str(),"z")==0) //FOUND z HEADING
-    cout << "FOUND Z HEADING!" << endl;
+  getline(inFileSon,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"x")==0) //FOUND son x HEADING
+    cout << "FOUND X HEADING! - " << tmpstring << endl;
   else
-    cout << "DIDN'T FIND Z HEADING!" << endl;
+    cout << "DIDN'T FIND X HEADING! - " << tmpstring << endl;
 
-  float * x_arr;
-  float * y_arr;
-  float * z_arr;
-  x_arr = new float[256];
-  y_arr = new float[256];
-  z_arr = new float[256];
+  getline(inFileSon,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"y")==0) //FOUND son y HEADING
+    cout << "FOUND Y HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND Y HEADING! - " << tmpstring << endl;
 
-  for(int i=0; i<256; i++)
+  getline(inFileSon,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"yaw")==0) //FOUND son yaw HEADING
+    cout << "FOUND YAW HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND YAW HEADING! - " << tmpstring << endl;
+
+  //Read in camera input file headings:
+  getline(inFileCam,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"t1")==0) //FOUND cam t1 HEADING
+    cout << "FOUND t1 HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND t1 HEADING! - " << tmpstring << endl;
+
+  getline(inFileCam,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"t2")==0) //FOUND cam t2 HEADING
+    cout << "FOUND t2 HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND t2 HEADING! - " << tmpstring << endl;
+
+  getline(inFileCam,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"x")==0) //FOUND cam x HEADING
+    cout << "FOUND x HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND x HEADING! - " << tmpstring << endl;
+
+  getline(inFileCam,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"y")==0) //FOUND cam y HEADING
+    cout << "FOUND y HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND y HEADING! - " << tmpstring << endl;
+
+  getline(inFileCam,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"z")==0) //FOUND cam z HEADING
+    cout << "FOUND z HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND z HEADING! - " << tmpstring << endl;
+
+  getline(inFileCam,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"roll")==0) //FOUND cam roll HEADING
+    cout << "FOUND roll HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND roll HEADING! - " << tmpstring << endl;
+
+  getline(inFileCam,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"pitch")==0) //FOUND cam pitch HEADING
+    cout << "FOUND pitch HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND pitch HEADING! - " << tmpstring << endl;
+
+  getline(inFileCam,tmpstring,';');
+  if(strcmp(tmpstring.c_str(),"yaw")==0) //FOUND cam yaw HEADING
+    cout << "FOUND yaw HEADING! - " << tmpstring << endl;
+  else
+    cout << "DIDN'T FIND yaw HEADING! - " << tmpstring << endl;
+
+  //Create input data arrays:
+  float * t1son_arr;
+  float * t2son_arr;
+  float * x_son_arr;
+  float * y_son_arr;
+  float * yaw_son_arr;
+  t1son_arr = new float[32];
+  t2son_arr = new float[32];
+  x_son_arr = new float[32];
+  y_son_arr = new float[32];
+  yaw_son_arr = new float[32];
+
+  float * t1cam_arr;
+  float * t2cam_arr;
+  float * xunit_arr;
+  float * yunit_arr;
+  float * zunit_arr;
+  float * roll_arr;
+  float * pitch_arr;
+  float * yaw_arr;
+  t1cam_arr = new float[32];
+  t2cam_arr = new float[32];
+  xunit_arr = new float[32];
+  yunit_arr = new float[32];
+  zunit_arr = new float[32];
+  roll_arr = new float[32];
+  pitch_arr = new float[32];
+  yaw_arr = new float[32];
+
+  //Read in the input data:
+  for(int i=0; i<32; i++)
     {
-      getline(inFile,tmpstring,';');
-      x_arr[i] = (float)(atof(tmpstring.c_str()));
-      getline(inFile,tmpstring,';');
-      y_arr[i] = (float)(atof(tmpstring.c_str()));
-      getline(inFile,tmpstring,';');
-      z_arr[i] = (float)(atof(tmpstring.c_str()));
+      //Sonar Data:
+      getline(inFileSon,tmpstring,';');
+      t1son_arr[i] = (float)(atof(tmpstring.c_str()));
+      getline(inFileSon,tmpstring,';');
+      t2son_arr[i] = (float)(atof(tmpstring.c_str()));
+
+      getline(inFileSon,tmpstring,';');
+      x_son_arr[i] = (float)(atof(tmpstring.c_str()));
+      getline(inFileSon,tmpstring,';');
+      y_son_arr[i] = (float)(atof(tmpstring.c_str()));
+      getline(inFileSon,tmpstring,';');
+      yaw_son_arr[i] = (float)(atof(tmpstring.c_str()));
+
+      //Camera Data:
+      getline(inFileCam,tmpstring,';');
+      t1cam_arr[i] = (float)(atof(tmpstring.c_str()));
+      getline(inFileCam,tmpstring,';');
+      t2cam_arr[i] = (float)(atof(tmpstring.c_str()));
+
+      getline(inFileCam,tmpstring,';');
+      xunit_arr[i] = (float)(atof(tmpstring.c_str()));
+      getline(inFileCam,tmpstring,';');
+      yunit_arr[i] = (float)(atof(tmpstring.c_str()));
+      getline(inFileCam,tmpstring,';');
+      zunit_arr[i] = (float)(atof(tmpstring.c_str()));
+    
+      getline(inFileCam,tmpstring,';');
+      roll_arr[i] = (float)(atof(tmpstring.c_str()));
+      getline(inFileCam,tmpstring,';');
+      pitch_arr[i] = (float)(atof(tmpstring.c_str()));
+      getline(inFileCam,tmpstring,';');
+      yaw_arr[i] = (float)(atof(tmpstring.c_str()));
     }    
-  for(int i=0; i<256; i++)
+
+  for(int i=0; i<32; i++)
     {
-      cout << x_arr[i] << "," << y_arr[i] << "," << z_arr[i] << endl;
+      cout << "x,y,z=" <<  xunit_arr[i] << "," << yunit_arr[i] << "," << zunit_arr[i] << endl;
+      cout << "roll,pitch,yaw=" << roll_arr[i] << "," << pitch_arr[i] << "," << yaw_arr[i] << endl;
+      cout << "xson,yson,yawson=" << x_son_arr[i] << "," << y_son_arr[i] << "," << yaw_son_arr[i] << endl;
     }
 
   // Create an empty nonlinear factor graph
   NonlinearFactorGraph graph;
 
-  double priorYaw= 0;
-  double priorPitch = 0;
-  double priorRoll = 0;
-  Rot3 priorMeanRot3 = Rot3::ypr(priorYaw, priorPitch, priorRoll);
+  Rot3 priorMeanRot3 = Rot3::ypr(yaw_arr[0], pitch_arr[0], roll_arr[0]);
 
   cout << "Prior Rot Matrix:" << endl;
   priorMeanRot3.print();
+
   Vector3 current_ypr;
   current_ypr = priorMeanRot3.ypr();
   cout << "Prior Yaw = " << current_ypr[0] << endl;
@@ -152,7 +264,10 @@ int main(int argc, char** argv)
   Point3 priorMeanPoint3(0,0,0);
   priorMeanPoint3.print("Prior 3D Point = ");
 
-  Pose3 priorMean(priorMeanRot3,priorMeanPoint3);
+  //  Pose3 priorMean(priorMeanRot3,priorMeanPoint3);
+  Pose3 priorMean(Rot3::ypr(0,0,0), Point3(0,0,0));
+  cout << "priorMean=" << endl;
+  priorMean.print();
 
   // Add a prior on the first pose, setting it to the origin
   // A prior factor consists of a mean and a noise model (covariance matrix)
@@ -171,29 +286,28 @@ int main(int argc, char** argv)
 
   // For simplicity, we will use the same noise model for each odometry factor
   //  noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Sigmas((Vector(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
-  noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Variances((Vector(6) << 1, 1, 1, 1, 1, 1));
+  noiseModel::Diagonal::shared_ptr sonarNoise = noiseModel::Diagonal::Variances((Vector(6) << 1, 1, 1, 1, 1, 1));
   // Create odometry (Between) factors between consecutive poses
 
-  Rot3 zeroRot3 = Rot3::ypr(0, 0, 0);
-  Unit3 xUnit3(1,0,0);
-  EssentialMatrix e_matrix(zeroRot3, xUnit3);
+  //EssentialMatrix e_matrix(zeroRot3, xUnit3);
+  EssentialMatrix e_matrix(Rot3::ypr(yaw_arr[0],pitch_arr[0],roll_arr[0]), Unit3(xunit_arr[0],yunit_arr[0],zunit_arr[0]));
 
-  noiseModel::Diagonal::shared_ptr eMatNoise = noiseModel::Diagonal::Variances((Vector(5) << 0.1, 0.1, 0.1, 0.1, 0.1));
+  noiseModel::Diagonal::shared_ptr cameraNoise = noiseModel::Diagonal::Variances((Vector(5) << 0.1, 0.1, 0.1, 0.1, 0.1));
 
   /*********************** Add Camera Nodes *************************/
 
   int i;
   for(i = 1; i<10; i++)
     {
-      graph.add(BetweenFactor<Pose3>(i, i+1, odometry_x, odometryNoise));
+      graph.add(BetweenFactor<Pose3>(i, i+1, Pose3(Rot3::ypr(yaw_son_arr[i],0,0), Point3(x_son_arr[i],y_son_arr[i],0)), sonarNoise));
     }
     for(i = 1; i<10; i++)
     {
-      graph.add(EssentialMatrixConstraint(i, i+1, e_matrix, eMatNoise));
+      graph.add(EssentialMatrixConstraint(i, i+1, EssentialMatrix(Rot3::ypr(yaw_arr[i],pitch_arr[i],roll_arr[i]), Unit3(xunit_arr[i],yunit_arr[i],zunit_arr[i])), cameraNoise));
     }
   int n = i-1;
 
-  graph.print("\nFactor Graph:\n"); // print
+  //graph.print("\nFactor Graph:\n"); // print
 
   // Create the data structure to hold the initialEstimate estimate to the solution
   // For illustrative purposes, these have been deliberately set to incorrect values
