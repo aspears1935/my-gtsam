d8c106611446e4d8fdb0378efd83b82056b9b84b
Anthony <aspears@gatech.edu>
2015-07-14 16:26:36 -0400

    changed xlsx to xls

--- cam_sonar_fusion/runAllAntarctica.sh
+++ cam_sonar_fusion/runAllAntarctica.sh
@@ -1,61 +1,61 @@
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty1.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty1.csv >> outputsAntarctica/consoleout_jetty1.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty1.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty1.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty1.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty2.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty2.csv >> outputsAntarctica/consoleout_jetty2.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty2.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty2.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty2.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty3a.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty3a.csv >> outputsAntarctica/consoleout_jetty3a.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty3a.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty3a.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty3a.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty3c.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty3c.csv >> outputsAntarctica/consoleout_jetty3c.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty3c.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty3c.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty3c.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty4.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty4.csv >> outputsAntarctica/consoleout_jetty4.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty4.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty4.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty4.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty5.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty5.csv >> outputsAntarctica/consoleout_jetty5.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty5.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty5.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty5.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty6.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty6.csv >> outputsAntarctica/consoleout_jetty6.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty6.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty6.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty6.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty7.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty7.csv >> outputsAntarctica/consoleout_jetty7.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty7.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty7.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty7.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty8.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty8.csv >> outputsAntarctica/consoleout_jetty8.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty8.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty8.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty8.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty9.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty9.csv >> outputsAntarctica/consoleout_jetty9.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty9.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty9.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty9.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty10.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty10.csv >> outputsAntarctica/consoleout_jetty10.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty10.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty10.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty10.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty11a.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty11a.csv >> outputsAntarctica/consoleout_jetty11a.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty11a.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty11a.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty11a.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty11c.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty11c.csv >> outputsAntarctica/consoleout_jetty11c.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty11c.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty11c.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty11c.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty11e.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty11e.csv >> outputsAntarctica/consoleout_jetty11e.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty11e.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty11e.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty11e.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsAntarctica/outputSonarGTSAM_jetty11g.csv ../../opencv-as/shiftEstVideoOnly/outputsAntarctica/outputCameraGTSAM_jetty11g.csv >> outputsAntarctica/consoleout_jetty11g.txt
 cp outputGTSAM.csv outputsAntarctica/outputGTSAM_jetty11g.csv
-cp outputGTSAM.xlsx outputsAntarctica/outputGTSAM_jetty11g.xlsx
+cp outputGTSAM.xls outputsAntarctica/outputGTSAM_jetty11g.xls
 
 
