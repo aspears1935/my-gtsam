e3e3765e45eac13ea2ad099d751e8753de79f420
Anthony <aspears@gatech.edu>
2015-07-14 16:26:28 -0400

    changed xlsx to xls

--- cam_sonar_fusion/runAllColorado.sh
+++ cam_sonar_fusion/runAllColorado.sh
@@ -1,107 +1,107 @@
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado1.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado1.csv >> outputsColorado/consoleout_colorado1.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado1.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado1.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado1.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado2.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado2.csv >> outputsColorado/consoleout_colorado2.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado2.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado2.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado2.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado3.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado3.csv >> outputsColorado/consoleout_colorado3.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado3.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado3.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado3.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado4.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado4.csv >> outputsColorado/consoleout_colorado4.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado4.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado4.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado4.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado5.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado5.csv >> outputsColorado/consoleout_colorado5.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado5.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado5.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado5.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado6.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado6.csv >> outputsColorado/consoleout_colorado6.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado6.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado6.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado6.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado7.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado7.csv >> outputsColorado/consoleout_colorado7.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado7.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado7.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado7.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado8.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado8.csv >> outputsColorado/consoleout_colorado8.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado8.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado8.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado8.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado9.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado9.csv >> outputsColorado/consoleout_colorado9.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado9.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado9.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado9.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado10.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado10.csv >> outputsColorado/consoleout_colorado10.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado10.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado10.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado10.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado11.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado11.csv >> outputsColorado/consoleout_colorado11.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado11.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado11.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado11.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado12.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado12.csv >> outputsColorado/consoleout_colorado12.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado12.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado12.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado12.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado13.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado13.csv >> outputsColorado/consoleout_colorado13.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado13.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado13.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado13.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado14.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado14.csv >> outputsColorado/consoleout_colorado14.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado14.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado14.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado14.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado15.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado15.csv >> outputsColorado/consoleout_colorado15.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado15.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado15.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado15.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado16.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado16.csv >> outputsColorado/consoleout_colorado16.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado16.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado16.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado16.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado17.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado17.csv >> outputsColorado/consoleout_colorado17.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado17.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado17.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado17.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado18.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado18.csv >> outputsColorado/consoleout_colorado18.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado18.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado18.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado18.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado19.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado19.csv >> outputsColorado/consoleout_colorado19.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado19.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado19.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado19.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado20.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado20.csv >> outputsColorado/consoleout_colorado20.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado20.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado20.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado20.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado21.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado21.csv >> outputsColorado/consoleout_colorado21.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado21.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado21.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado21.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado22.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado22.csv >> outputsColorado/consoleout_colorado22.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado22.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado22.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado22.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado23.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado23.csv >> outputsColorado/consoleout_colorado23.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado23.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado23.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado23.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado24.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado24.csv >> outputsColorado/consoleout_colorado24.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado24.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado24.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado24.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado25.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado25.csv >> outputsColorado/consoleout_colorado25.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado25.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado25.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado25.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado26.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado26.csv >> outputsColorado/consoleout_colorado26.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado26.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado26.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado26.xls
 
 ./CamSonarFusion ../../opencv-as/shiftEstSonarOnly/outputsColorado/outputSonarGTSAM_colorado27.csv ../../opencv-as/shiftEstVideoOnly/outputsColorado/outputCameraGTSAM_colorado27.csv >> outputsColorado/consoleout_colorado27.txt
 cp outputGTSAM.csv outputsColorado/outputGTSAM_colorado27.csv
-cp outputGTSAM.xlsx outputsColorado/outputGTSAM_colorado27.xlsx
+cp outputGTSAM.xls outputsColorado/outputGTSAM_colorado27.xls
